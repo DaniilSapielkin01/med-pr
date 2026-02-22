0a6228033bb44ff01d1525a12a082e9175340e6c
Danil <danil@MacBook-Air-Danil.local>
2026-02-22 20:43:46 +0200

    Fix: fixed build

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -3,5 +3,4 @@ export { TextInput } from "./text-input";
 export { MyAccordion as Accordion } from "./accordion";
 export { SharedDialog } from "./dialog";
 export { BgCircleMain } from "./bg-circle-main";
-export { ChatBot } from "./chat-bot";
 export { default as ChatBox } from "./chat-bot-v2/ChatBox";
